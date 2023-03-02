3e2216653aef0e12a371cf6b9eb469ffcb8bcd93
Sarthak Neupane <sarthakrocks2004@gmail.com>
2023-03-02 18:42:23 +0545

    reverted to default

--- plugins/socket.io.client.js
+++ plugins/socket.io.client.js
@@ -2,9 +2,11 @@ import io from 'socket.io-client'
 
 console.log(location.host)
 // const socket = io(`${protocol}://${location.host}`)
-const socket = io(`https://${location.host}:10000`)
+// const socket = io(`https://${location.host}:10000`)
 // const socket = io(`http://0.0.0.0:3000`)
 // const socket = io('http://0.0.0.0:10000')
+
+const socket = io('http://localhost:8000')
 
 export default defineNuxtPlugin((NuxtApp) => {
     return {
