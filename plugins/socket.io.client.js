4b9ce790da0098ae4e8cfd01cc466e5f4c13d7a3
Sarthak Neupane <sarthakrocks2004@gmail.com>
2023-03-02 17:03:08 +0545

    another one

--- plugins/socket.io.client.js
+++ plugins/socket.io.client.js
@@ -2,8 +2,9 @@ import io from 'socket.io-client'
 
 console.log(location.host)
 // const socket = io(`${protocol}://${location.host}`)
-const socket = io(`http://${location.host}:8000`)
+const socket = io(`https://${location.host}:10000`)
 // const socket = io(`http://0.0.0.0:3000`)
+// const socket = io('http://0.0.0.0:10000')
 
 export default defineNuxtPlugin((NuxtApp) => {
     return {
